2aa10b793cd4fe18103bb9fbd4138f63dafdeb18
fl4tisjustice <chaoticflat@gmail.com>
2023-12-02 10:53:14 +0000

    Day Two: Part Two

--- Day 2/cubes2.py
+++ Day 2/cubes2.py
@@ -1,28 +1,22 @@
 import re
+from functools import reduce
 
 with open('cubes_input.txt') as fl:
     games = fl.readlines()
-
-target = {'red': 12, 'green': 13, 'blue': 14}
 
 result = 0
 
 for i, game in enumerate(games, 1):
     game = re.sub('Game [0-9]+: ', '', game.rstrip('\n')).split('; ')
     game = map(lambda x: x.split(', '), game)
-    possible = True
+    minimum = dict()
     for cubeset in game:
-        current = target.copy()
         for cube in cubeset:
             num, color = cube.split(' ')
             num = int(num)
-            if current.get(color):
-                current[color] -= num
-        if any(num < 0 for num in current.values()):
-            possible = False
-            break
+            minimum[color] = max(minimum.get(color, 0), num)
 
-    result += i if possible else 0
-
+    result += reduce(lambda x, y: x * y, minimum.values())
+    
 print(result)
             
